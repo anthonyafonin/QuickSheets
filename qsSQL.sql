1a8ce9a564368942508360460401e2e4c8d7a449
anthonyafonin <anthonyafonin@gmail.com>
2017-02-03 16:14:46 -0800

    Redesigned Database

--- qsSQL.sql
+++ qsSQL.sql
@@ -1,56 +1,41 @@
-create database android_api /** Creating Database **/
- 
-use android_api /** Selecting Database **/
- 
-create table users(
-   id int(11) primary key auto_increment,
-   unique_id varchar(23) not null unique,
-   name varchar(50) not null,
-   email varchar(100) not null unique,
-   encrypted_password varchar(80) not null,
-   salt varchar(10) not null,
-   created_at datetime,
-   updated_at datetime null
-); /** Creating Users Table **/
-
-
 --
--- Table structure for table `employee`
+-- Table structure for table `user_account`
 --
-CREATE TABLE employee (
-   Employee_ID INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE user_account (
+   Account_ID INT(5) NOT NULL AUTO_INCREMENT,
    First_Name VARCHAR (20) NOT NULL,
    Middle_Name VARCHAR (20),
    Last_Name VARCHAR (20) NOT NULL,
    Phone_Number VARCHAR (20) NOT NULL,
-   Account_ID INT(11) NOT NULL,
- PRIMARY KEY (Employee_ID),
-    KEY Users (Account_ID)
+   Email VARCHAR (40) NOT NULL,
+   Time_Sheet_ID INT(5) NOT NULL,
+ PRIMARY KEY (Account_ID),
+    KEY Sheets (Time_Sheet_ID)
 );
 
 --
 -- Table structure for table `time_sheet`
 --
 CREATE TABLE time_sheet (
-   Time_Sheet_ID INT(11) NOT NULL AUTO_INCREMENT,
-   Hours INT(11) NOT NULL,
-   _Date DATE NOT NULL,
-   _Month VARCHAR (15) NOT NULL,
-   Job_Type VARCHAR (20) NOT NULL,
-   Customer VARCHAR (20) NOT NULL,
-   Description VARCHAR (20) NOT NULL,
-   Employee_ID INT(11) NOT NULL,
+   Time_Sheet_ID INT(5) NOT NULL AUTO_INCREMENT,
+   Title VARCHAR (25) NOT NULL,
+   Start_Date DATETIME NOT NULL,
+   End_Date DATETIME NOT NULL,
+   TS_Entry_ID INT(5) NOT NULL,
  PRIMARY KEY (Time_Sheet_ID),
-    KEY Employee (Employee_ID)
+    KEY Contents (TS_Entry_ID)
 );
 
 --
--- Table structure for table `account`
+-- Table structure for table `ts_entry`
 --
-CREATE TABLE account (
-   Account_ID INT(11) NOT NULL AUTO_INCREMENT,
-   Account_Type VARCHAR (15) NOT NULL,
-   Username VARCHAR (20) NOT NULL,
-   Password VARCHAR (20) NOT NULL,
- PRIMARY KEY (Account_ID)
+CREATE TABLE ts_entry (
+   TS_Entry_ID INT(5) NOT NULL AUTO_INCREMENT,
+   Job_Type VARCHAR (20),
+   Customer VARCHAR (20) NOT NULL,
+   Description VARCHAR (50) NOT NULL,
+   Hours INT(2) NOT NULL,
+   TS_Date DATE NOT NULL,
+ PRIMARY KEY (TS_Entry_ID),
 );
+
